f0f0c46c77b043859a5abd9fca7268cf49169ce4
taakr <167289139+taakr@users.noreply.github.com>
2024-05-29 15:57:42 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+// JavaScript
+console.log('Hello!!!!');
+
+//背景色を変える
+function MyFunctionR(){
+    const obj = document.body;
+    obj.style.backgroundColor = '#fee6ef';
+  }
+  
+  function MyFunctionG(){
+    const obj = document.body;        
+    obj.style.backgroundColor = '#fee7c0';
+  }
+  
+  function MyFunctionB(){
+    const obj = document.body;
+    obj.style.backgroundColor = '#e6fbfe';
+  }
+
+  //引数を用いた背景色の変更
+function MyFunction05( color ){
+    const obj = document.body;
+    obj.style.backgroundColor = color;
+  }
+
+  //ランダムな16進6桁のカラーコードを生成（汎用性のある関数）
+function randomColor() {
+    let col = Math.floor(Math.random() * 0xFFFFFF).toString(16);
+    for (let i = col.length; i < 6; i++) {
+      col = '0' + col;
+    }
+    return '#' + col;
+  }
+  
+  // 背景色の変更
+  function MyFunction06(){
+    const obj = document.body;
+    obj.style.backgroundColor = randomColor();
+  }
+
+  
